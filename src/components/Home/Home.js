19667c3ec56940354c725e1089ba7b856dd92f88
kitlingmui <kitlingmui@gmail.com>
2019-01-03 19:56:12 -0800

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+
+    return (
+      <>
+        <h1>This is my Budget App Home Page</h1>
+      </>
+    );
+  }
+}
+
+export default Home;
+
