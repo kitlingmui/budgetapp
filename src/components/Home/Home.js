8d1719d0bf27c0e1fe2c455008dd9dcc812c03d7
unknown <taylordarnell89@yahoo.com>
2019-01-05 13:18:23 -0800

    newUpload

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,64 +1,29 @@
 import React, { Component } from 'react';
-import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-});
-
+import Piggy from './homeComponents/piggy.js'
+import Text from './homeComponents/text.js'
+import Login from './homeComponents/loginBtn.js'
+import Create from './homeComponents/createActBtn.js'
+import InputEmail from './homeComponents/inputFormEmail.js'
+import InputPass from './homeComponents/inputFormPass.js'
+import Terms from './homeComponents/terms.js'
 
 class Home extends Component {
   render() {
-
     return (
       <>
-<Grid
-  container
-  direction="row"
-  justify="center"
-  alignItems="center"
->
-
-  <Grid item xs>
-    <div>   
-        <img src="./images/piggy.jpg"/>
-        <h4>15 Bucks little man...</h4>
-    </div>
-      <br/>
-      <br/>
-    <div>
-      <form> 
-        <Input placeholder='Email' />
-          <br/>
-          <br/>
-        <Input placeholder='Password'/>
-      </form>
-          <br/>
-          <br/>
-        <Button variant="contained" color="primary">
-          Login
-        </Button>
-          <br/>
-          <br/>
-        <Button variant="contained" color="primary">
-          Create Account
-        </Button>
-    </div>
-    </Grid>
-  </Grid>   
-       
+      <Piggy/>
+      <Text/>
+      <InputEmail/>
+      <InputPass/>
+      <Login/>
+      <Create/>
+      <Terms/>
       </>
     );
   }
 }
 
 export default Home;
+
+
 
