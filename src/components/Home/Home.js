7182cafe7f92afeddd0f5906a1a12f3feb9f5153
unknown <taylordarnell89@yahoo.com>
2019-01-05 10:25:14 -0800

    upload

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
 
 class Home extends Component {
   render() {
 
     return (
       <>
-        <h1>This is my Budget App Home Page</h1>
+<Grid
+  container
+  direction="row"
+  justify="center"
+  alignItems="center"
+>
+
+  <Grid item xs>
+    <div>   
+        <img src="./images/piggy.jpg"/>
+        <h4>15 Bucks little man...</h4>
+    </div>
+      <br/>
+      <br/>
+    <div>
+      <form> 
+        <Input placeholder='Email' />
+          <br/>
+          <br/>
+        <Input placeholder='Password'/>
+      </form>
+          <br/>
+          <br/>
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
+          <br/>
+          <br/>
+        <Button variant="contained" color="primary">
+          Create Account
+        </Button>
+    </div>
+    </Grid>
+  </Grid>   
+       
       </>
     );
   }
