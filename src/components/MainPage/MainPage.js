1e16d62a7c4aaee5cd90540349ed47749da39f26
kitlingmui <kitlingmui@gmail.com>
2019-01-05 14:58:59 -0800

    kit update1

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import Navbar from '../Navbar'
 
 const styles = theme => ({
-    
-  });
+  root: {
+    flexGrow: 1,
+  },   
+});
+
+class MainPage extends Component {
+  render() {
+    return (
+      <>
+       <Navbar/ > 
+       <div>
+         <h1>MainPage Component to be update</h1>
+       </div>
+      </>
+    );
+  }
+}
+  
 export default MainPage;
