38a4f2c283d2b1e16a064d445f9cdb6759cec3b0
kitlingmui <kitlingmui@gmail.com>
2019-01-05 14:59:21 -0800

    kit update1

---
+++ src/components/Budget/Budget.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Navbar from '../Navbar'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },   
+});
+
+class Budget extends Component {
+  render() {
+    return (
+      <>
+       <Navbar/ > 
+       <div>
+         <h1>Budget Component to be update</h1>
+       </div>
+      </>
+    );
+  }
+}
+  
+export default Budget;
