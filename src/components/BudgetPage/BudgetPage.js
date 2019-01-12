a6fdad43b97ac9a5b9a9fc5dd4a080f5cefe5d79
kitlingmui <kitlingmui@gmail.com>
2019-01-12 11:04:36 -0800

    kit update 011219

---
+++ src/components/BudgetPage/BudgetPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Navbar from '../Navbar'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },   
+});
+
+class BudgetPage extends Component {
+  render() {
+    return (
+      <>
+       <Navbar/ > 
+       <div>
+         <h1>Budget Page Component to be update</h1>
+       </div>
+      </>
+    );
+  }
+}
+  
+export default BudgetPage;
